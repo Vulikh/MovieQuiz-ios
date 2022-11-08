66740e137c13ba0ab0a5b9f093b09be8ec652111
Vulikh <fed.vul@icloud.com>
2022-11-08 21:40:21 +0300

    исправлены замечания

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -66,7 +66,7 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
-    @IBOutlet var noButton: UIButton!
+    @IBOutlet private var noButton: UIButton!
     @IBOutlet var yesButton: UIButton!
     
     private var currentQuestionIndex: Int = 0
